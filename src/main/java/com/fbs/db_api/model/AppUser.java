3b5f238725e7cdd2340dd0916bc2e2ea4dba72dc
nishant920 <nishantdhiman044@gmail.com>
2025-07-12 16:25:05 +0530

    started new project

--- src/main/java/com/fbs/db_api/model/AppUser.java
+++ src/main/java/com/fbs/db_api/model/AppUser.java
@@ -1,15 +1,15 @@
 package com.fbs.db_api.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
+@Entity
 public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     UUID Id;
+    @Column
     String Email;
     String Name;
     String Password;
